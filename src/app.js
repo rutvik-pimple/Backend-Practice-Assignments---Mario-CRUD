a8bc9b9c38f0ed08cf05d34acd9b50497ce6ddaf
rutvik_pimple <rdp1621998@gmail.com>
2020-12-08 22:51:09 +0530

    ...'.......

--- src/app.js
+++ src/app.js
@@ -22,9 +22,6 @@ app.get('/mario',(req,res)=>{
 
 app.get('/mario/:id',(req,res)=>{
     const marioId = req.params.id;
-    if (!marioId){
-        res.status(400).send({ message: error.message })
-    }
     marioModel.find({ _id: marioId })
     .then((mariochars) =>
       mariochars.map((mariochars) => res.send(mariochars))
